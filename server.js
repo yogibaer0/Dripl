51549544343baf82358b82ffab2244b92f6e5be5
yogibaer0 <vbgray02@gmail.com>
2025-10-03 20:30:20 -0500

    chore: temporarily remove helmet to unblock deploy

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import os from "node:os";
 import crypto from "node:crypto";
 import express from "express";
 import cors from "cors";
-import helmet from "helmet";
+// import helmet from "helmet";
 import morgan from "morgan";
 import rateLimit from "express-rate-limit";
 import { spawn } from "node:child_process";
@@ -177,7 +177,7 @@ function cookieSummary() {
 // ===== App =====
 const app = express();
 app.use(cors());
-app.use(helmet({ crossOriginResourcePolicy: false }));
+// app.use(helmet({ crossOriginResourcePolicy: false }));
 app.use(express.json({ limit: "1mb" }));
 app.use(morgan("tiny"));
 app.use(rateLimit({ windowMs: 60_000, max: 100, legacyHeaders: false }));
